79ad9dcd345c46f452cd43f24521f101cb773a6b
pdp <lutfullaevrakhmiddin@gmail.com>
2026-02-25 18:59:50 +0500

    3

---
+++ src/LeetCode3.java
@@ -0,0 +1,42 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LeetCode3 {
+    public static void main(String[] args) {
+        String s = " ";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+    public static int lengthOfLongestSubstring(String s) {
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            Set<Character> set = new HashSet<>();
+            for (int j = i; j < s.length(); j++) {
+                if (set.contains(s.charAt(j))) {
+                    break;
+                }
+                set.add(s.charAt(j));
+            }
+            if (max < set.size()) {
+                max = set.size();
+            }
+        }
+        return max;
+    }
+
+    public static int lengthOfLongestSubstring1(String s) {
+        Set<Character> set = new HashSet<>();
+        int left = 0;
+        int max = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left++));
+                left++;
+            }
+            set.add(s.charAt(right));
+            max = Math.max(max, right - left + 1);
+        }
+
+        return max;
+    }
+}
